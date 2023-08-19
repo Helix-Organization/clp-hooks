e7cdd52d6d1523eb3c4ed4a5a49c806ef672ef69
wonj1012 <jjaa1012@gmail.com>
2023-08-19 22:22:25 +0900

    ✨feat: Define constants

---
+++ constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+// Token pair
+#define TOKEN_A "TokenA"
+#define TOKEN_B "TokenB"
+
+// Fee rate
+#define FEE_RATE 0.003 // 0.3% fee
+
+// Transaction types
+#define DEPOSIT_TRANSACTION   0x01
+#define WITHDRAW_TRANSACTION  0x02
+#define SWAP_TRANSACTION      0x03
+
+#endif // CONSTANTS_H
